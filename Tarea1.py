0146a31c7f7b61456235370875bd34bbb2f835a7
Liam Salazar <salazar.martinez.liamantonio@gmail.com>
2025-05-10 22:24:46 -0600

    Commit

--- Tarea1.py
+++ Tarea1.py
@@ -9,32 +9,61 @@ def leerDocumentos(carpeta):
                 contenido_total += archivo.read()  
     return contenido_total
 
-def eliminar_stopwords(texto, stop_words):
+def eliminar_stopwords(texto):
     palabras = texto.split()
     palabras_filtradas = [palabra for palabra in palabras if palabra not in stop_words]
     return palabras_filtradas
 
-def eliminar_caracteres_especiales(texto, caracteres):
+def eliminar_caracteres_especiales(texto):
     for caracter in caracteres:
         texto = texto.replace(caracter, '')
     return texto
 
+def eliminar_acentos(texto):
+    acentos = {
+        'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u'
+    }
+    for acento, sin_acento in acentos.items():
+        texto = texto.replace(acento, sin_acento)
+    return texto
+
+def normalizar_texto(texto):
+    texto = texto.lower()  
+    texto = eliminar_caracteres_especiales(texto) 
+    texto = eliminar_acentos(texto) 
+    texto_normalizado = eliminar_stopwords(texto) 
+    return texto_normalizado
+
+def crearDiccionario(texto_normalizado):
+    diccionario = {}
+    for palabra in texto_normalizado:
+        if palabra in diccionario:
+            diccionario[palabra] += 1
+        else:
+            diccionario[palabra] = 1
+    return diccionario
+
+def palabras_mas_frecuentes(diccionario, n):
+    palabras_ordenadas = sorted(diccionario.items(), key=lambda x: x[1], reverse=True)
+    return palabras_ordenadas[:n]
 
 carpeta = "./ArchivosTarea"  
 texto_normalizado = []
 stop_words = [
     'de', 'la', 'que', 'el', 'en', 'y', 'a', 'los', 'del', 'se',
     'las', 'por', 'un', 'para', 'con', 'no', 'una', 'su', 'al',
-    'lo', 'como', 'más', 'pero', 'sus', 'le', 'ya', 'o',
-    'sí', 'porque', 'muy', 'sin', 'también', 'me', 'hasta',
+    'lo', 'como', 'mas', 'pero', 'sus', 'le', 'ya', 'o',
+    'porque', 'muy', 'sin', 'también', 'hasta',
     'donde', 'todo', 'nos', 'uno', 'ni', 'yo', 'tú', 'él',
     'ella', 'nosotros', 'ellos', 'ellas', 'mi', 'tu', 'te',
-    'me', 'sí', 'nos', 'ese', 'esa', 'esto', 'eso'
+    'me', 'si', 'nos', 'ese', 'esa', 'esto', 'eso', 'es', 'son', 'he', 'has', 'han', 'había'
 ]
 caracteres = [',', '.', '!', '?', '¿', '¡', ':', ';', '-', '_', '(', ')', '[', ']', '{', '}', "'", '"']
 
 texto = leerDocumentos(carpeta)
-texto = texto.lower() 
-texto = eliminar_caracteres_especiales(texto, caracteres)
-texto_normalizado = eliminar_stopwords(texto, stop_words)
-print(texto_normalizado)
+texto_normalizado = normalizar_texto(texto)
+print("Palabras más frecuentes:")
+diccionario = crearDiccionario(texto_normalizado)
+palabras_frecuentes = palabras_mas_frecuentes(diccionario, 5)
+for palabra, frecuencia in palabras_frecuentes:
+    print(f"{palabra}: {frecuencia}")
